8ddd985d2397f0f9b6ff2c1684de0b832560ffce
Radiyah <rsabur58@gmail.com>
2021-09-13 13:18:43 -0400

    Working

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { Card } from "semantic-ui-react"
 import "./styles.css";
 
+function App() {
+  const [events, setEvents] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/events')
+      .then(res => res.json())
+      .then(eventArr => {
+        setEvents(eventArr)
+        setIsLoaded(true)
+      })
+  }, [])
+
+  if (!isLoaded) return <h2>Loading...</h2>
+  
+  console.log(events)
+  // const renderEvents = events.map(event => <Card keys={event.id + event.name} {...event} />)
+  return (
+    <div>Working...</div>
+    // {renderEvents}
+  )
+}
+
+const element = <App />
 ReactDOM.render(
-  <div>Add your code here!</div>,
+  element,
   document.getElementById("root-container")
 );
