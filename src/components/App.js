a476a5e3a266dbc56a73b9407d365a568da4bc20
Radiyah <rsabur58@gmail.com>
2021-09-22 17:11:28 -0400

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
 import Events from './Events'
 
 
 function App() {
     const [events, setEvents] = useState([])
     const [isLoaded, setIsLoaded] = useState(false)
+    const [pageNum, setPageNum] = useState(1) 
 
     useEffect(() => {
-        fetch('http://localhost:3000/events')
+        fetch(`http://localhost:3000/events?page=${pageNum}`)
             .then(res => res.json())
             .then(eventsObj => {
                 setEvents(eventsObj.events)
@@ -16,15 +18,25 @@ function App() {
     }, [])
 
     if (!isLoaded) return <h2>Loading...</h2>
-    console.log(events);
+    // console.log(events);
 
     const renderEvents = events.map(event => <Events key={event.id + event.title} {...event} />)
 
+    const loadMoreEvents = () => {
+        console.log('loading...')
+    }
 
     return (
         <div className='app'>
             <div className='events'>
                 {renderEvents}
+            </div>
+            <div className='button'>
+                <Button
+                    variant='secondary'
+                    onClick={() => loadMoreEvents()}>
+                        Load More
+                </Button>
             </div>
         </div>
     )
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,19 +1,49 @@
-import React from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
 
 function Events({ id, title, event_start, event_end, image_url, venue }) {
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    var eventStart = new Date(event_start)
+    var eventEnd = new Date(event_end)
+
+    const eventDate = 
+
 
     return (
-            <Card>
-                <Image src={image_url} wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>title</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>{event_start}</span>
-                    </Card.Meta>
-                </Card.Content>
+        <>
+            <Card style={{ width: '22rem', margin: '1rem' }} onClick={handleShow}>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {eventStart.toDateString()},
+                    </Card.Text>
+                </Card.Body>
             </Card>
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h5><strong>Event Details</strong></h5><br />
+                    <p>
+                        <strong>Date & Time</strong><br />
+                        {eventStart.toDateString()} - {eventEnd.toDateString()}<br />
+                        {eventStart.toLocaleTimeString('en-US')} - {eventEnd.toLocaleTimeString('en-US')}<br />
+                        <br /><strong>Location</strong><br />
+
+                    </p>
+                </Modal.Body>
+            </Modal>
+        </>
     )
 }
 
